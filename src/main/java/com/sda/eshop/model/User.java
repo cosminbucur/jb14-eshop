2145a5617afc1d574016290d9e7000f4478bf0d1
Marius D <mariusdelcea88@gmail.com>
2020-08-29 13:15:28 +0300

    Added user - model

--- src/main/java/com/sda/eshop/model/User.java
+++ src/main/java/com/sda/eshop/model/User.java
@@ -3,33 +3,30 @@ package com.sda.eshop.model;
 
 import javax.persistence.*;
 
-@Entity
-@Table
+@Entity(name = "User")
+@Table(name = "users")
 public class User {
 
-
-    @Column(name = "name")
-    String name;
-    @Column(name = "username")
-    String username;
-    @Column(name = "password")
-    String password;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private long id;
+    private Long id;
 
+    @Column(name = "name")
+    private String name;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
 
-    public User(long id, String username) {
-        this.id = id;
-        this.username = username;
+    public User() {
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -57,6 +54,7 @@ public class User {
         this.password = password;
     }
 
+    //todo Hash password
     @Override
     public String toString() {
         return "User{" +
@@ -67,5 +65,25 @@ public class User {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        User user = (User) o;
+
+        if (!getId().equals(user.getId())) return false;
+        if (!getName().equals(user.getName())) return false;
+        if (!getUsername().equals(user.getUsername())) return false;
+        return getPassword().equals(user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId().hashCode();
+        result = 31 * result + getName().hashCode();
+        result = 31 * result + getUsername().hashCode();
+        result = 31 * result + getPassword().hashCode();
+        return result;
+    }
 }
